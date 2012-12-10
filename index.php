4d0330c82a7afd2b01d81646e7503568f16a48a1
PrzemyslawMalinowski <przemyslaw.malinowski@uj.edu.pl>
2012-12-10 23:19:06 +0100

    pierwszy komit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+
+<!-- CZĘŚĆ DEKLARACYJNA -->
+<head>
+	<meta name="keywords" content="słowa, kluczowe, rozdzielone, przecinkami"/>
+    <meta name="description" content="Witryna"/>
+    <meta name="author" content="Przemysław Malinowski"/>
+    <meta name="generator" content="Eclipse"/>
+    <meta name="copyright" content="Przemysław Malinowski"/>
+    <meta name="language" content="pl"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF8">
+	<title>Tytuł</title>
+	
+	<style type="text/css">
+	/* <![CDATA[ */
+	@import url('styles/style.css');
+	/* ]]> */
+	</style>
+
+</head>
+
+<!-- CZĘŚĆ WYKONAWCZA -->
+<body>
+    
+    <?php include "script.php";?>
+	
+</body>
+
+</html>
